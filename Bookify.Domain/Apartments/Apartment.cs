abdee86fff66baeef1dc756e08850747f899a55e
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-20 12:30:37 +0330

    implement apartment entity

---
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,18 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public sealed class Apartment
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public string Country { get; private set; }
+    public string State { get; private set; }
+    public string ZipCode { get; private set; }
+    public string City { get; private set; }
+    public string Street { get; private set; }
+    public decimal PriceAmont { get; private set; }
+    public string PriceCurrency { get; private set; }
+    public decimal CleaningFeeAmont { get; private set; }
+    public DateTime? LastBookedOnUtc { get; private set; }
+    public List<Amenity> Amenities { get; private set; } = new();
+}
