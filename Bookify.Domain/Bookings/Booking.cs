9c9e31173edae4138ebe8e5126e25507916b0f7c
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-21 12:40:23 +0330

    implement booking entity

---
+++ Bookify.Domain/Bookings/Booking.cs
@@ -0,0 +1,35 @@
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Bookings;
+
+public class Booking : Entity
+{
+    private Booking(Guid id, Guid apartmentId, Guid userId, DateRange duration, Money priceForPeriod, Money cleaningFee,
+        Money amenitiesUpCharge, Money totalPrice, BookingStatus status, DateTime createOnUtc) : base(id)
+    {
+        ApartmentId = apartmentId;
+        UserId = userId;
+        Duration = duration;
+        PriceForPeriod = priceForPeriod;
+        CleaningFee = cleaningFee;
+        AmenitiesUpCharge = amenitiesUpCharge;
+        TotalPrice = totalPrice;
+        Status = status;
+        CreateOnUtc = createOnUtc;
+    }
+
+    public Guid ApartmentId { get; private set; }
+    public Guid UserId { get; private set; }
+    public DateRange Duration { get; private set; }
+    public Money PriceForPeriod { get; private set; }
+    public Money CleaningFee { get; private set; }
+    public Money AmenitiesUpCharge { get; private set; }
+    public Money TotalPrice { get; private set; }
+    public BookingStatus Status { get; private set; }
+    public DateTime CreateOnUtc { get; private set; }
+    public DateTime? ConfirmedOnUtc { get; private set; }
+    public DateTime? RejectedOnUtc { get; private set; }
+    public DateTime? CompletedOnUtc { get; private set; }
+    public DateTime? CancelledOnUtc { get; private set; }
+}
