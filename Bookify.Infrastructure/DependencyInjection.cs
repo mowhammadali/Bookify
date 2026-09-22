8a1f6a1152df92bb7ee8fc8e3de79c0481bf9f89
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-22 14:58:26 +0330

    add ef core and postgressql

--- Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Bookify.Application.Abstractions.Email;
 using Bookify.Infrastructure.Clock;
 using Bookify.Infrastructure.Email;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,6 +14,9 @@ public static class DependencyInjection
     {
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
         services.AddTransient<IEmailService, EmailService>();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("Database")).UseSnakeCaseNamingConvention());
 
         return services;
     }
