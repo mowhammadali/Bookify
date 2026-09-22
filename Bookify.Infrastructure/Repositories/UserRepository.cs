562ac2debb328de2ff084d79ef20e4d2d0329cde
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-22 19:34:36 +0330

    implement repositories in infrastructure layer

---
+++ Bookify.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Bookify.Domain.Users;
+
+namespace Bookify.Infrastructure.Repositories;
+
+internal sealed class UserRepository : Repository<User>, IUserRepository
+{
+    public UserRepository(ApplicationDbContext dbContext)
+        : base(dbContext)
+    {
+    }
+
+    public void AddUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
