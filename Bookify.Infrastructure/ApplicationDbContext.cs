7bc29916c2be8d44f7aff4be4660562064234d7e
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-22 15:09:30 +0330

    implement entity configuration for EF Core

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -8,4 +8,10 @@ public class ApplicationDbContext : DbContext, IUnitOfWork
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 }
